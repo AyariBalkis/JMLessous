d96f5b89f8d3a2e2964af19888ee8e70e719fe9e
belki <ayari.balkis@esprit.tn>
2022-10-11 15:49:43 +0200

    Initial commit

---
+++ src/main/java/com/jmlessous/JmLessousApplication.java
@@ -0,0 +1,13 @@
+package com.jmlessous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmLessousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmLessousApplication.class, args);
+    }
+
+}
